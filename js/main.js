d9679eff43ce1b821a52c636a713da61def00a74
Sudar Abisheck <sudarabisheck@gmail.com>
2016-08-10 18:00:30 +0530

    bug: fixed absolute positioning bug

--- js/main.js
+++ js/main.js
@@ -9,28 +9,36 @@ console.log("Viewport width and height : " + x + ", " + y);
 
 d.getElementById('container').style.height = y + "px";
 
-var contHeight = d.getElementById('container').clientHeight;
-console.log(contHeight);
+function absolutePositioning() {
+    var contHeight = d.getElementById('container').clientHeight;
+    console.log(contHeight);
 
-//setting elements absolute top position
-d.getElementById('c1').style.top =  contHeight - d.getElementById('c1').clientHeight + "px";
-d.getElementById('c2').style.top =  contHeight - d.getElementById('c2').clientHeight + "px";
-d.getElementById('bush1').style.top =  contHeight - d.getElementById('bush1').clientHeight + "px";
-d.getElementById('bush2').style.top =  (contHeight*0.97) - d.getElementById('bush2').clientHeight + "px";
-d.getElementById('ssn').style.top =  (contHeight*0.94) - d.getElementById('ssn').clientHeight + "px";
-d.getElementById('gate').style.top =  (contHeight*0.96) - d.getElementById('gate').clientHeight + "px";
-d.getElementById('sun').style.top =  contHeight - d.getElementById('sun').clientHeight + "px";
-d.getElementById('logo').style.top =  (contHeight*0.03) + "px";
+    //setting elements absolute top position
+    d.getElementById('c1').style.top =  contHeight - d.getElementById('c1').clientHeight + "px";
+    d.getElementById('c2').style.top =  contHeight - d.getElementById('c2').clientHeight + "px";
+    d.getElementById('bush1').style.top =  contHeight - d.getElementById('bush1').clientHeight + "px";
+    d.getElementById('bush2').style.top =  (contHeight*0.97) - d.getElementById('bush2').clientHeight + "px";
+    d.getElementById('ssn').style.top =  (contHeight*0.94) - d.getElementById('ssn').clientHeight + "px";
+    d.getElementById('gate').style.top =  (contHeight*0.96) - d.getElementById('gate').clientHeight + "px";
+    d.getElementById('sun').style.top =  contHeight - d.getElementById('sun').clientHeight + "px";
+    d.getElementById('logo').style.top =  (contHeight*0.03) + "px";
 
-var contWidth = d.getElementById('container').clientWidth;
-console.log(contWidth);
-var conWidthHalf = contWidth / 2;
-//setting elements absolute left position
-d.getElementById('c1').style.left =  conWidthHalf - d.getElementById('c1').clientWidth / 2 + "px";
-d.getElementById('c2').style.left =  conWidthHalf - d.getElementById('c2').clientWidth / 2+ "px";
-d.getElementById('bush1').style.left =  conWidthHalf - d.getElementById('bush1').clientWidth  / 2 + "px";
-d.getElementById('bush2').style.left =  conWidthHalf - d.getElementById('bush2').clientwidth / 2+ "px";
-d.getElementById('ssn').style.left =  conWidthHalf - d.getElementById('ssn').clientWidth / 2+ "px";
-d.getElementById('gate').style.left =  conWidthHalf - d.getElementById('gate').clientWidth / 2 + "px";
-d.getElementById('sun').style.left =  conWidthHalf - d.getElementById('sun').clientwidth / 2 + "px";
-d.getElementById('logo').style.left =  conWidthHalf - d.getElementById('logo').clientwidth / 2 + "px";
+    var contWidth = d.getElementById('container').clientWidth;
+    console.log(contWidth);
+    var conWidthHalf = contWidth / 2;
+    //setting elements absolute left position
+    d.getElementById('c1').style.left =  conWidthHalf - d.getElementById('c1').clientWidth / 2 + "px";
+    d.getElementById('c2').style.left =  conWidthHalf - d.getElementById('c2').clientWidth / 2+ "px";
+    d.getElementById('bush1').style.left =  conWidthHalf - d.getElementById('bush1').clientWidth  / 2 + "px";
+    d.getElementById('bush2').style.left =  conWidthHalf - d.getElementById('bush2').clientwidth / 2+ "px";
+    d.getElementById('ssn').style.left =  conWidthHalf - d.getElementById('ssn').clientWidth / 2+ "px";
+    d.getElementById('gate').style.left =  conWidthHalf - d.getElementById('gate').clientWidth / 2 + "px";
+    d.getElementById('sun').style.left =  conWidthHalf - d.getElementById('sun').clientWidth / 2 + "px";
+    d.getElementById('logo').style.left =  conWidthHalf - d.getElementById('logo').clientWidth / 2 + "px";
+};
+
+absolutePositioning();
+
+window.onresize = function(event){
+    absolutePositioning();
+}
